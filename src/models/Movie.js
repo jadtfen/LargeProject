afab95c3dc3df03ff9c09bd21f8ea2f6c88e0fe4
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-07-25 00:08:40 -0400

    Update Movie.js

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  movieID: { type: Number, unique: true },
+  movieID: { type: Number, required: true, unique: true },
   title: { type: String, required: true },
   genre: { type: String, required: true },
   description: { type: String, required: true },
 });
 
-const Movie = mongoose.model('Movie', movieSchema, 'movie'); // Ensure 'movie' is the correct collection name
+const Movie = mongoose.model('Movie', movieSchema, 'movie');
 
 module.exports = Movie;
