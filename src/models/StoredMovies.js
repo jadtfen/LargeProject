ad283244eddd051928e66d0abf4a7ed042474351
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-07-25 00:10:47 -0400

    Create StoredMovies.js

---
+++ src/models/StoredMovies.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const storedMoviesSchema = new mongoose.Schema({
+  userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  movieID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Movie',
+    required: true,
+  },
+  partyID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Party',
+    required: true,
+  },
+  votes: { type: Number, default: 0 },
+  watchedStatus: { type: Boolean, default: false },
+});
+
+// Create the StoredMovies model
+const StoredMovies = mongoose.model('StoredMovies', storedMoviesSchema);
+
+module.exports = StoredMovies;
