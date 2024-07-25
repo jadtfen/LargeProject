96fb19d922f3d38b1ede13ebe8edefbc4c512c61
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-07-25 00:10:07 -0400

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
@@ -14,18 +13,4 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-// Method to verify password
-userSchema.methods.verifyPassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
-
-// Hook to hash password before saving
-userSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  }
-  next();
-});
-
-module.exports = mongoose.model('user', userSchema, 'users');
+module.exports = mongoose.model('User', userSchema, 'users');
