127ca1aee4af360698b990d6ad2d65a82a105bce
jadtfen <jjaden562@gmail.com>
2024-07-25 01:36:48 -0400

    Landing

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/LandingPage.css';
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './styles/LandingPage.css';
-
 const LandingPage = () => {
   return (
     <div className="landing-container">
